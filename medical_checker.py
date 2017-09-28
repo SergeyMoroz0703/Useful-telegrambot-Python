4af1eeff50fb7275a5149f1c667d55f488455949
sergeymoroz <sergeymoroz0307@gmail.com>
2017-09-28 17:27:48 +0300

    added full msg

--- medical_checker.py
+++ medical_checker.py
@@ -3,11 +3,9 @@ import re
 import requests
 import time
 from bs4 import BeautifulSoup
+import sys
 
-
-dicts = {}
-#u_choice = input("Введите имя препарата: ")
-u_choice = 'Солпадеин'
+u_choice = 'Нафтизин'
 
 class Tracker():
 
@@ -62,7 +60,11 @@ class Tracker():
         list = []
         names = soup.find('div', class_='goog-trans-section')
         global all_el
-        all_el = names.find_all(class_=None)
+        try:
+            all_el = names.find_all(class_=None)
+        except AttributeError:
+            print('Не корректное название лекарства')
+            return False
         for i in range(0,len(all_el)):
             if all_el[i] == soup.find('h2', id='Состав'):
                 list.append(i)
@@ -88,57 +90,155 @@ class Tracker():
         anti_indications = list[2]
         method_to_eat = list[3]
         affects = list[4]
-        # list_substance =[]                                                                                #this part of code is for MSG about substance
-        # z = substance
-        # while z < (indications-1):
-        #     z = z+1
-        #     msg = all_el[z].text
-        #     #print(msg)
-        #     #time.sleep(1)
-        #     if msg == 'діюча речовина:' or \
-        #                     msg == 'допоміжні речовини:' or \
-        #                     msg == 'допоміжні речовини: ' or \
-        #                     msg == 'діючі речовини: ' or \
-        #                     msg == 'діючі речовини:' or \
-        #                     msg == 'Основні фізико-хімічні властивості:' or \
-        #                     msg == 'Основні фізико-хімічні властивості: ' or \
-        #                     msg == 'Абсорбція' or \
-        #                     msg == 'Розподіл.' or \
-        #                     msg =='Біотрансформація.' or \
-        #                     msg == 'Виведення.':
-        #         pass
-        #     else:
-        #         list_substance.append(msg+'\n')
-        # print(list_substance)
-        # msg_substance = []
-        # for i in list_substance:
-        #     if i not in msg_substance:
-        #         msg_substance.append(i)
-        # msg_substance = ''.join(msg_substance)
-        # print(msg_substance)                                                                              #       end here
+        list_substance =[]                                              #this part of code is for MSG about substance
+        z = substance
+        while z < (indications-1):
+            z = z+1
+            msg = all_el[z].text
+            if msg == 'діюча речовина:' or \
+                            msg == 'допоміжні речовини:' or \
+                            msg == 'допоміжні речовини: ' or \
+                            msg == 'діючі речовини: ' or \
+                            msg == 'діючі речовини:' or \
+                            msg == 'Основні фізико-хімічні властивості:' or \
+                            msg == 'Основні фізико-хімічні властивості: ' or \
+                            msg == 'Абсорбція' or \
+                            msg == 'Розподіл.' or \
+                            msg =='Біотрансформація.' or \
+                            msg == '®' or \
+                            msg == '® ' or \
+                            msg == 'Виведення.':
+                pass
+            else:
+                list_substance.append(msg+'\n')
+        #print(list_substance)
+        msg_substance = []
+        for i in list_substance:
+            if i not in msg_substance:
+                msg_substance.append(i)
+        msg_substance = ''.join(msg_substance)
+        print(msg_substance)                                                                         #       end here
 
 
-        list_indications = []
+        list_indications = []                                           # this part of code for MSG about indications
         x = indications -1
         while x < anti_indications -1:
             x = x+1
             msg1 = all_el[x].text
             list_indications.append(msg1 + '\n')
-
         msg_indications = []
-        print(list_indications)
+        for i in list_indications:
+            if i not in msg_indications:
+                msg_indications.append(i)
+        msg_indications = ''.join(msg_indications)
+        print(msg_indications)                                                    #end here
 
 
+        list_anti_indications = []                                 # this part of code for MSG about antiindications
+        c = anti_indications -1
+        while c < method_to_eat -1:
+            c = c+1
+            msg2 = all_el[c].text
+            if msg2 == 'Спричинена вмістом парацетамолу:' or \
+                            msg2 == 'Спричинена вмістом кодеїну: ' or \
+                            msg2 == 'Спричинена вмістом кофеїну: ' or \
+                            msg2 == 'Здатність впливати на швидкість реакції при керуванні автотранспортом або ' \
+                                    'іншими механізмами.' or \
+                            msg2 == 'Застосування у період вагітності або годування груддю.' or \
+                            msg2 == 'Вагітність. ' or \
+                            msg2 == 'Годування груддю. ' or \
+                            msg2 == '®' or \
+                            msg2 == 'Дослідження in vitro.' or \
+                            msg2 == '® ' or \
+                            msg2 == ' in vitro.' or \
+                            msg2 == 'in vitro' or \
+                            msg2 == 'Дослідження in vivo.' or \
+                            msg2 == ' in vivo.' or \
+                            msg2 == 'in vivo.' or \
+                            msg2 == 'Підвищена чутливість до активної речовини або будь-якої з допоміжних' \
+                                    ' речовин препарату.' or \
+                            msg2 == '50' or \
+                            msg2 == 'Втрата слуху.' or \
+                            msg2 == 'Захворювання, що передаються статевим шляхом.' or \
+                            msg2 == 'Вплив на кровотечі. ' or \
+                            msg2 == '®' or \
+                            msg2 == 'Вплив на зір. ':
+
+               pass
+            else:
+                list_anti_indications.append(msg2+'\n')
+        print(list_anti_indications)
+        msg_anti_indications = []
+        for i in list_anti_indications:
+            if i not in msg_anti_indications:
+                msg_anti_indications.append(i)
+        msg_anti_indications = ''.join(msg_anti_indications)
+        print(msg_anti_indications)                                                     # end here
 
 
+        list_meth_eat = []
+        v = method_to_eat -1
+        while v < affects -1:
+            v = v+1
+            msg3 = all_el[v].text
+            if msg3 == 'Дорослі:' or \
+                msg3 == 'Дорослі: ' or \
+                msg3 == 'Діти. ' or \
+                msg3 == 'Діти.' or \
+                msg3 == 'Симптоми передозування парацетамолом.' or \
+                msg3 == 'Діти' or \
+                msg3 == 'Симптоми передозування кофеїном. ' or \
+                msg3 == 'Спосіб введення.' or \
+                msg3 == '®' or \
+                msg3 == '® ' or \
+                msg3 == '®\xa0' or \
+                msg3 == 'Симптоми передозування кодеїном.':
+                pass
+            else:
+                list_meth_eat.append(msg3+'\n')
+        print(list_meth_eat)
+        msg_meth_eat = []
+        for i in list_meth_eat:
+            if i not in msg_meth_eat:
+                msg_meth_eat.append(i)
+        msg_meth_eat = ''.join(msg_meth_eat)
+        print(msg_meth_eat)
 
-
-        #return msg_substance
-
+        list_affects = []
+        b = affects -1
+        while b < len(all_el) -13: #len(all_el) -2:
+            b = b+1
+            msg4 = all_el[b].text
+            if msg4 == 'З боку кровоносної та лімфатичної системи:' or \
+                msg4 == 'З боку імунної системи:' or \
+                msg4 == 'З боку шкіри та підшкірної тканини:' or \
+                msg4 == 'Порушення з боку гепатобіліарної системи:' or \
+                msg4 == 'З боку центральної нервової системи:' or \
+                msg4 == 'Психічні порушення:' or \
+                msg4 == 'З боку серця:' or \
+                msg4 == 'З боку нирок і сечовидільної системи' or \
+                msg4 == 'Інші:' or \
+                msg4 == 'Порушення з боку органів дихання, грудної клітини та середостіння:' or \
+                msg4 == '® ' or \
+                msg4 == 'з боку нервової системи' or \
+                msg4 == 'З боку дихальної системи, органів грудної клітки та середостіння:' or \
+                msg4 == 'з боку серцево-судинної системи' or \
+                msg4 == 'З боку травного тракту:':
+                pass
+            else:
+                list_affects.append(msg4+'\n')
+        msg_affects = []
+        for i in list_affects:
+            if i not in msg_affects:
+                msg_affects.append(i)
+        msg_affects = ''.join(msg_affects)
+        print(msg_affects)
 
 
 track = Tracker()
 track.get_msg_substance()
-
+# if track.eat_method() == False:
+#     print('False')
+# print(track.eat_method()[3])
 
 
